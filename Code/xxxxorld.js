ad28d765c67ad5e63238ab52de2255b7588b7fef
Arjun Pandit <arjunpndt.edu@gmail.com>
2023-10-12 10:24:44 +0530

    Create xxxxorld.js

---
+++ Code/xxxxorld.js
@@ -0,0 +1,2 @@
+const greetings = ["Hola Mundo!", "Bonjour le Monde!", "नमस्ते दुनिया!", "Ciao Mondo!", "Hallo Welt!", "Привет, мир!", "こんにちは、世界！", "Hello World", "안녕하세요, 세계!", "你好，世界！", "Salamu, Dunia!", "Merhaba Dünya!", "Hei maailma!", "Hej världen!", "Прывітанне, свет!", "Halo, Dunia!"];
+console.log(greetings[Math.floor(Math.random() * greetings.length)]);
